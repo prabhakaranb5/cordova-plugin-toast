15510e209e1642caa7eb10fd0b31e26ac38736f1
prabhakaranb5 <41184241+prabhakaranb5@users.noreply.github.com>
2025-08-04 15:32:22 +0300

    Update ToastPlugin.java

--- src/android/ToastPlugin.java
+++ src/android/ToastPlugin.java
@@ -1,27 +1,51 @@
-package com.example.toast;
+package com.ferdinandsilva.android;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
 import android.widget.Toast;
-import org.apache.cordova.*;
+
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
-public class ToastPlugin extends CordovaPlugin {
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("show".equals(action)) {
-            String message = args.getString(0);
-            String duration = args.getString(1);
-            showToast(message, duration);
-            callbackContext.success();
-            return true;
-        }
-        return false;
+public class SimpleToastPlugin extends CordovaPlugin {
+    public static final String TAG = "SimpleToastPlugin";
+    public static Context thisContext;
+
+    public SimpleToastPlugin() {
     }
 
-    private void showToast(final String message, final String duration) {
-        cordova.getActivity().runOnUiThread(() -> {
-            int toastDuration = "long".equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT;
-            Toast.makeText(cordova.getActivity(), message, toastDuration).show();
-        });
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        SimpleToastPlugin.thisContext = cordova.getActivity().getApplicationContext();
+    }
+    
+
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+        if ("show".equals(action)) {
+            int toastDuration = 0;
+
+            if(args.getInt(1) == 1) {
+                toastDuration = Toast.LENGTH_LONG;
+            } else {
+                toastDuration = Toast.LENGTH_SHORT;
+            }
+            Toast.makeText(SimpleToastPlugin.thisContext,args.get(0).toString(), toastDuration).show();
+            callbackContext.success(args.get(0).toString());
+        } else {
+            return false;
+        }
+
+        return true;    
     }
 }
