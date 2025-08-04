0d6684d93d986d5c197a6049c241b68b5d8d26ca
prabhakaranb5 <41184241+prabhakaranb5@users.noreply.github.com>
2025-08-04 15:32:49 +0300

    Update toast.js

--- www/toast.js
+++ www/toast.js
@@ -1,5 +1,11 @@
-var exec = require('cordova/exec');
+"use strict";
 
-exports.show = function(message, duration, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, 'ToastPlugin', 'show', [message, duration]);
+var exec = require("cordova/exec");
+
+var simpleToastPlugin = {
+	show: function(txt, duration, sc, ec) {
+		exec(sc, ec, "SimpleToastPlugin", "show", [txt, duration]);
+	}
 };
+
+module.exports = simpleToastPlugin;
